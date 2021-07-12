6354b5cede192b45c4481d41d4231b023b435d17
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2021-07-12 11:13:43 +0200

    Cleaned the Karatsuba Algorithm

--- Karatsuba Algorithm.py
+++ Karatsuba Algorithm.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[83]:
-
-
 def karatsuba_algorithm(f_num, s_num):
     """
     An Algorithmic function that returns the multiplication between two numbers
@@ -29,39 +23,21 @@ def karatsuba_algorithm(f_num, s_num):
         return f_num * s_num
     else:
         n = max(len(str(f_num)), len(str(s_num))) // 2
-        a = f_num // 10**(n)
-        #print(a)
-        b = f_num % 10**(n)
-        #print(b)
-        c = s_num // 10**(n)
-        #print(c)
-        d = s_num % 10**(n)
-        #print(d)
+        a = f_num // 10 ** n
+        b = f_num % 10 ** n
+        c = s_num // 10 ** n
+        d = s_num % 10 ** n
         
         ac = karatsuba_algorithm(a, c)
-        bd = karatsuba_algorithm(b,d)
+        bd = karatsuba_algorithm(b, d)
         a_plus_b_multi_c_plus_d = karatsuba_algorithm(a+b, c+d) - ac - bd
         
         product = (ac * 10**(2 * n)) + (a_plus_b_multi_c_plus_d * 10**(n)) + bd
         return product
 
-
-# In[84]:
-
-
-karatsuba_algorithm(5678,1234)
-
-
-# In[79]:
-
-
+x = 5678
+y = 1234
+print("The product of\n {} * {} = {}\n".format(x, y, karatsuba_algorithm(x, y)))
 x = 3141592653589793238462643383279502884197169399375105820974944592
 y = 2718281828459045235360287471352662497757247093699959574966967627
-karatsuba_algorithm(x, y)
-
-
-# In[80]:
-
-
-karatsuba_algorithm(123,345)
-
+print("The product of\n {} * {} = {}\n".format(x, y, karatsuba_algorithm(x, y)))
